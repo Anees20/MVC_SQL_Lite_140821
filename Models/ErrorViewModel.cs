914616bd9760c2bb0108df2e579a706924dcd4f3
Anees <mohd.anees2019@outlook.com>
2021-08-15 22:13:35 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
